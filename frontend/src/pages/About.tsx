706d30857b17718e4182f07fa0a6bd891fcde7b5
Arquake <arquake2@gmail.com>
2025-01-02 21:41:27 +0100

    PATCH about responsive
    Ajout et modifications de quelques éléments dans le about pour le rendre plus responsive

--- frontend/src/pages/About.tsx
+++ frontend/src/pages/About.tsx
@@ -1,25 +1,27 @@
 export default function About() {
   return (
-    <div className="container py-12 px-6 lg:px-16 mx-auto">
-      <div className="bg-neutral-100 p-8 rounded-xl">
-        <h1 className="text-4xl font-bold text-blue-600 mb-6 text-left">
-          À propos
+    <div className="container">
+      <div className="pt-8 md:px-2 px-8">
+        <h1 className="lg:text-3xl text-2xl font-bold text-blue-600 pb-4 tracking-tight">
+          Qu'est ce que Creacosm ?
         </h1>
-        <p className="text-lg text-neutral-700 leading-relaxed">
-          Créacosm est une marque française de cosmétiques innovants, créée par
-          des étudiants de l’Université d’Orléans. Issue d’une collaboration
-          unique avec des enseignants-chercheurs et des industriels locaux, elle
-          a pour mission de concevoir des produits qui allient science, nature
-          et beauté, tout en respectant l’environnement et des valeurs éthiques.
-          Grâce à une recherche continue et à des ingrédients soigneusement
-          sélectionnés, Créacosm propose des solutions cosmétiques qui célèbrent
-          une beauté naturelle et durable.
-        </p>
-        <p className="text-lg text-neutral-700 leading-relaxed mt-6">
-          Cet escape game a été conçu pour célébrer le lancement d'une nouvelle
-          gamme de produits par Créacosm, combinant divertissement et innovation
-          pour marquer cet événement exceptionnel.
-        </p>
+        <div className="indent-8 lg:text-xl sm:text-lg text-neutral-900 space-y-6 sm:leading-6 leading-5 opacity-90">
+          <p>
+            Créacosm est une marque française de cosmétiques innovants, créée par
+            des étudiants de l’Université d’Orléans. Issue d’une collaboration
+            unique avec des enseignants-chercheurs et des industriels locaux, elle
+            a pour mission de concevoir des produits qui allient science, nature
+            et beauté, tout en respectant l’environnement et des valeurs éthiques.
+            Grâce à une recherche continue et à des ingrédients soigneusement
+            sélectionnés, Créacosm propose des solutions cosmétiques qui célèbrent
+            une beauté naturelle et durable.
+          </p>
+          <p>
+            Cet escape game a été conçu pour célébrer le lancement d'une nouvelle
+            gamme de produits par Créacosm, combinant divertissement et innovation
+            pour marquer cet événement exceptionnel.
+          </p>
+        </div>
       </div>
     </div>
   );
